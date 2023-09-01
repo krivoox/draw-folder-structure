175ff9e03ab9652b7caeba00f7be3d64a1c14486
Juan Krivocapich <u633559@T0003547080.local>
2023-09-01 20:22:24 -0300

    initial proyect

---
+++ README.md
@@ -0,0 +1,41 @@
+# Markdown Folder Structure for VS Code
+
+![Banner o Logo de la Extensión](ruta/a/tu/imagen/banner.png)
+
+Una extensión de Visual Studio Code que permite a los usuarios generar una representación visual en Markdown de la estructura de carpetas y archivos seleccionados.
+
+## Características
+
+- **Generación Rápida**: Con un solo clic, obtén una representación en Markdown de tu estructura de carpetas.
+- **Formato Atractivo**: La estructura generada utiliza caracteres Unicode para una visualización clara y estética.
+- **Integración Perfecta**: Se integra directamente en el menú contextual del explorador de archivos de VS Code.
+
+## Uso
+
+1. Navega al explorador de archivos en VS Code.
+2. Haz clic derecho en una carpeta o archivo.
+3. Selecciona "Generar estructura en Markdown" del menú contextual.
+4. ¡Disfruta de tu estructura de carpetas en formato Markdown!
+
+## Capturas de Pantalla
+
+![Uso Básico]('.assets/screen01.png')
+_Descripción de la imagen_
+
+![Otra Funcionalidad]('.assets/screen02.png')
+_Descripción de la imagen_
+
+## Instalación
+
+1. Abre Visual Studio Code.
+2. Ve a la vista de Extensiones o presiona `Ctrl+Shift+X`.
+3. Busca "Markdown Folder Structure".
+4. Instala y ¡listo!
+
+## Contribuciones
+
+Las contribuciones son bienvenidas. Si encuentras algún problema o tienes una sugerencia, por favor, abre un issue en el repositorio de GitHub.
+
+## Licencia
+
+[MIT](LICENSE)
---
+++ src/extension.ts
@@ -0,0 +1,59 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+
+export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand(
+    'extension.generateMarkdownStructure',
+    (folder: vscode.Uri) => {
+      const folderPath = folder.fsPath;
+      const itemName = path.basename(folderPath);
+      const stats = fs.statSync(folderPath);
+      let markdownStructure = '';
+
+      if (stats.isDirectory()) {
+        markdownStructure += `└── ${itemName}\n`;
+        markdownStructure += generateStructure(folderPath, 1);
+      } else {
+        markdownStructure = `├── ${itemName}\n`;
+      }
+
+      vscode.workspace
+        .openTextDocument({ content: markdownStructure, language: 'markdown' })
+        .then((doc) => {
+          vscode.window.showTextDocument(doc);
+        });
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+function generateStructure(
+  dir: string,
+  depth: number = 0,
+  isFile: boolean = false
+): string {
+  const indent = '  '.repeat(depth); // 4 espacios para simular una tabulación
+  let structure = '';
+
+  if (isFile) {
+    return `${indent}├── ${path.basename(dir)}\n`;
+  }
+
+  const items = fs.readdirSync(dir);
+  for (const item of items) {
+    const itemPath = path.join(dir, item);
+    const stats = fs.statSync(itemPath);
+    if (stats.isDirectory()) {
+      structure += `${indent}└── ${item}\n`;
+      structure += generateStructure(itemPath, depth + 1);
+    } else {
+      structure += generateStructure(itemPath, depth + 1, true);
+    }
+  }
+
+  return structure;
+}
+
+export function deactivate() {}
